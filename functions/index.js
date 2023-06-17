54584fcfe3fd5a35e0f9e05b825f6364de2a9786
Maleeki <slyman7311@gmail.com>
2023-06-17 22:51:58 +0200

    yes

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 const functions = require('firebase-functions');
 const cors = require('cors')({ origin: true});
-const admin = require('firebase-admin');
+const admin = require('firebase-admin/app');
 const serviceAccount = require('./service-account.json');
 
 admin.initializeApp({
@@ -8,4 +8,24 @@ admin.initializeApp({
     databaseURL: "https://dumbai-c3561-default-rtdb.europe-west1.firebasedatabase.app/",
 });
 
-const { sessionsClient } = require('dialogflow');
+const { sessionsClient, SessionsClient } = require('dialogflow');
+
+
+exports.dialogflowGateway = functions.https.onRequest((request, response) => {
+    cors(request, response, async () => {
+        const { queryInput, sessionId } = request.body;
+
+
+        const sessionClient = new SessionsClient({ credentials: serviceAccount });
+        const session = sessionClient.sessionPath('dumbai-c3561-default-rtdb', sessionId);
+
+
+        const responses = await sessionClient.detectIntent({ session, queryInput});
+
+        const result = responses[0].queryResult;
+
+        result.fulfillmentText
+
+        response.send(result);
+    })
+})
